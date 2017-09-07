827b747fbbed8357f25d0960180e8357ce216eca
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-09-07 01:13:05 -0700

    update

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 // keys.js - figure out what set of credentials to return
-if (process.env.NODE_ENV === "production") {
+const env = process.env.NODE_ENV || "development";
+
+if (env === "production") {
   // Heroku will set process.env
   module.exports = require('./prod');
 } else {
